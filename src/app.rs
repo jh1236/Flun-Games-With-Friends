141a340f73efb4cc002802884f687f5e2e1a3f2a
Jared Healy <healy_jared@yahoo.com>
2024-07-15 02:04:43 +0800

    It almost fully works

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,7 @@
 use std::borrow::ToOwned;
+use egui::Pos2;
+use egui::Shape::LineSegment;
+use egui::TextStyle::Button;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -16,10 +19,30 @@ pub struct TemplateApp {
     #[serde(skip)] // This how you opt-out of serialization of a field
     xs_turn: bool,
     #[serde(skip)] // This how you opt-out of serialization of a field
-    board: [usize; 9],
+    board: [i32; 9],
 }
 
-
+fn get_winner(board: &[i32; 9]) -> i32 {
+    for i in 0..3 {
+        if board[3 * i] == board[3 * i + 1] && board[3 * i + 1] == board[3 * i + 2] && board[3 * i] != 0 {
+            return board[3 * i];
+        }
+        if board[i] == board[3 + i] && board[3 + i] == board[6 + i] && board[i] != 0 {
+            return board[i];
+        }
+    }
+    for i in 0..2 {
+        if board[2 * i] == board[4] && board[4] == board[8 - 2 * i] && board[4] != 0 {
+            return board[4];
+        }
+    }
+    for i in 0..9 {
+        if board[i] == 0 {
+            return 0;
+        }
+    }
+    return -1;
+}
 
 
 impl Default for TemplateApp {
@@ -53,14 +76,9 @@ impl TemplateApp {
 }
 
 impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let index_to_symbol = ["-".to_owned(), "X".to_owned(), "O".to_owned()];
+        let index_to_symbol = [" - ".to_owned(), "X".to_owned(), "O".to_owned()];
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
@@ -85,7 +103,7 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("Time for Tick Tack Toe");
+            ui.heading("Time for Tic-Tac-Toe");
 
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
@@ -94,18 +112,30 @@ impl eframe::App for TemplateApp {
 
             ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
             for r in 0..3 {
-                ui.horizontal(|x| {
+                ui.horizontal(|ui| {
                     for c in 0..3 {
-                        if x.button(index_to_symbol[self.board[r * 3 + c]].to_owned()).clicked() {
-                            self.board[r * 3 + c] = 1 + if self.xs_turn { 1 } else { 0 };
-                            self.xs_turn = !self.xs_turn;
+                        let button = egui::Button::new(index_to_symbol[self.board[r * 3 + c] as usize].to_owned());
+                        if ui.add_sized([50., 50.], button).clicked() {
+                            if self.board[r * 3 + c] != 0 {
+                                //explode
+                            } else {
+                                self.board[r * 3 + c] = 1 + if self.xs_turn { 0 } else { 1 };
+                                self.xs_turn = !self.xs_turn;
+                            }
                         }
                     }
                 });
                 ui.separator();
             }
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            let winner = get_winner(&self.board);
+            if winner > 0 {
+                ui.label(index_to_symbol[winner as usize].to_owned() + "'s have won the game!");
+            } else if winner < 0 {
+                ui.label("It's a fockin' draw lassie");
+            }
+            if ui.button("Reset").clicked() {
+                self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+                self.xs_turn = true;
             }
 
             ui.separator();
@@ -116,23 +146,13 @@ impl eframe::App for TemplateApp {
             ));
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
             });
         });
     }
-}
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
+    /// Called by the framework to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
 }
